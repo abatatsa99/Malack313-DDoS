91766af3bef1a35d1401926f2c86bff587ad66d7
abatatsa99 <abatatsa311@gmail.com>
2025-10-27 08:21:31 +0700

    Update malack-ddos.py

--- malack-ddos.py
+++ malack-ddos.py
@@ -53,7 +53,7 @@ def ddos():
                 time.sleep(1)
                 attack_num += 1
                 print(f"{Fore.LIGHTWHITE_EX}[]  {Fore.LIGHTCYAN_EX}M A L A C K \033[103mSent massage\033[0m  \033[41m"+ str(attack_num)+"\033[0m \033[94m Starting\033[0")
-                print(f"{Fore.LIGHTRED_EX}[]  {Fore.LIGHTWHITE_EX}M A L A C K \033[7mSent massage\033[0m  \033[103m"+ str(attack_num)+"\033[0m \033[31m Starting\033[0m")
+                print(f"{Fore.LIGHTRED_EX}[]  {Fore.LIGHTBLUE_EX}M A L A C K \033[7mSent massage\033[0m  \033[103m"+ str(attack_num)+"\033[0m \033[31m Starting\033[0m")
             except socket.error:
                 time.sleep(1)
                 print(f"{Fore.BLUE}[]  {Fore.YELLOW}Connection failed   \033[7m please check host\033[0m")
